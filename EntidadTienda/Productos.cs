4ed877ee04be24f912537c432794296245251fc8
Sagid Hazael <sagid45@hotmail.com>
2022-09-17 21:04:21 -0500

    Se Creo la rama Altas

---
+++ EntidadTienda/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntidadTienda
+{
+    public class Productos
+    {
+        public Productos(int idproducto, string nombre, string descripcion, int precio)
+        {
+            this.idproducto = idproducto;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.precio = precio;
+        }
+
+        public int idproducto { get; set; }
+        public string nombre { get; set; }
+        public string descripcion { get; set; }
+        public int precio { get; set; }
+    }
+}
